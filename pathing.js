49c87553e5180d8707fd84d8b375a0e1f3d218aa
Stian Karlsen <logggi@users.noreply.github.com>
2017-06-30 21:46:20 +0200

    Update pathing.js

--- pathing.js
+++ pathing.js
@@ -1,7 +1,7 @@
 module.exports = {
-  run: function(creep, target, reuse) {
-  	if(creep.moveTo(target, { noPathFinding: true, reusePath: reuse }) == ERR_NO_PATH) {
+	run: function(creep, target, reuse) {
+  		if(creep.moveTo(target, { noPathFinding: true, reusePath: reuse }) == ERR_NO_PATH) {
 			creep.moveTo(target);
 		}
-  }
+	}
 };
