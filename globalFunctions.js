f3a4d6ab661fa48cda0b7f41213b30c045c1a3f0
Stian Karlsen <logggi@users.noreply.github.com>
2017-07-02 06:53:45 +0200

    Update globalFunctions.js

--- globalFunctions.js
+++ globalFunctions.js
@@ -36,7 +36,7 @@ module.exports = {
 	            var i = 0;
 	            Memory.spawnStructures = {};
 	            this.find(FIND_STRUCTURES, {filter: obj => obj.structureType == STRUCTURE_EXTENSION || obj.structureType == STRUCTURE_SPAWN}).forEach(function(obj) {;
-	                Memory.spawnStructures[i++] = {id: obj.id, pos: obj.pos};
+	                Memory.spawnStructures[i++] = {id: obj.id};
 	                Memory.spawnStructures.update = false;
 	                Memory.spawnStructures.length = i;
 	            });
